fff9f8c5709ff46750c5e5ef2a1aa0f906e9a2e0
vuslatsena <vuslatsena23@gmail.com>
2023-04-29 09:09:36 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+document.addEventListener("DOMContentLoaded", () => {
+  const task = document.querySelector('#new-task-description');
+  const todo = document.querySelector("#tasks");
+  const button = document.querySelector('#submit-button');
+
+  button.addEventListener('click', (event)=>{
+    event.preventDefault();
+    const adding = task.value;
+    const toadd = document.createElement("li");
+    toadd.textContent = adding;
+    todo.appendChild(toadd);
+    task.value="";
+  })
+});
 });
